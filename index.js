5588f1e94b37a1f12262d0724126b60b745a1200
Tomas Garcia <tomasgarciadev@gmail.com>
2022-01-11 17:19:18 +0100

    notes-app step 10

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const NotesModel = require('./model/notesModel')
+const NotesView = require('./model/notesView')
+
+// console.log doesn't print anything on the page
+// it is not meant to be visible to the user, but for you
+// to help in debugging and getting visibility in your JS code.
+//
+// on Mac (using Chrome), use Option+Command+J to open the console and see this message.
+
+
+
+const model = new NotesModel();
+model.addNote('This is an example note');
+
+
+const notesView = new NotesView(model);
+
+notesView.displayNotes();
